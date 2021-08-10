0af984f673ca78872672b19996b9d3dd39402d0a
Ben Luker <thebenluker@gmail.com>
2021-08-09 20:43:54 -0700

    Update sketch.js

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -4,10 +4,13 @@
 let particleDensity = 25;
 let connectionRange = 100;
 let fadeInTime = 1;
+let mouseStrength = 0.5;
+let mouseFalloff = 400;
 
 let parentID = 'animatedBackground';
 let footerID = 'footer';
 let particles = [];
+let dir;
 let opacity;
 
 function setup() {
@@ -24,12 +27,10 @@ function setup() {
   }
 }
 
-let resized = false;
 function draw() {
   opacity = min((millis() / (1000 * fadeInTime)), 1); 
-  let speed = abs(winMouseX - pwinMouseX);
-  let dirX = 
-
+  mouseVel = createVector(mouseX - pmouseX, mouseY - pmouseY);
+  
   clear();
   for(let i = 0;i<particles.length;i++) {
     particles[i].createParticle();
@@ -53,7 +54,7 @@ class Particle {
         this.y = random(0,height);
         this.r = random(1,8);
         this.xSpeed = random(-2,2);
-        this.ySpeed = random(-1,1.5);
+        this.ySpeed = random(-1.5,1.5);
       }
     
     // creation of a particle.
@@ -65,12 +66,23 @@ class Particle {
     
     // setting the particle in motion.
       moveParticle() {
-        if(this.x < 0 || this.x > width)
+        let dis = dist(this.x,this.y,mouseX,mouseY);
+        let influence = map(dis, 0, mouseFalloff, 1, 0, true);
+
+        // let mouseInfluence = createVector((abs(mouseVel.x) / mouseVel.x) * (min(abs(mouseVel.x), maxInfluence)), (abs(mouseVel.y) / mouseVel.y) * (min(abs(mouseVel.y), maxInfluence)));
+        if ((this.x < 0 && this.xSpeed < 0) || (this.x > width && this.xSpeed > 0))
           this.xSpeed*=-1;
-        if(this.y < 0 || this.y > height)
+        if ((this.y < 0 && this.ySpeed < 0) || (this.y > height && this.ySpeed > 0))
           this.ySpeed*=-1;
-        this.x+=this.xSpeed;
-        this.y+=this.ySpeed;
+        // if(this.x < 0 || this.x > width)
+        //   this.xSpeed*=-1;
+        // if(this.y < 0 || this.y > height)
+        //   this.ySpeed*=-1;
+        // this.xSpeed += dir.x;
+        // this.ySpeed += dir.y; 
+        
+        this.x+=this.xSpeed + (mouseVel.x * mouseStrength * influence);
+        this.y+=this.ySpeed + (mouseVel.y * mouseStrength * influence);
       }
     
     // this function creates the connections(lines)
